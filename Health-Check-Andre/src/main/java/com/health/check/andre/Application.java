32e912b33b3ad881daf6a5327e7c514cc1400a64
andre <andrepraca@gmail.com>
2021-08-19 18:31:41 -0300

    Adicionando novo projeto.

---
+++ Health-Check-Andre/src/main/java/com/health/check/andre/Application.java
@@ -0,0 +1,11 @@
+package com.health.check.andre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
